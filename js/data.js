03d7f90d8f895d6a289defdec309edb4a9ac1b3a
edinson_figueroa <figueroaedinson@gmail.com>
2021-07-17 14:37:12 -0500

    en proceso

---
+++ js/data.js
@@ -0,0 +1,57 @@
+export const data = [{
+  id: '608f52b3782d44c311f7a49a',
+  img:'./img/portafolio-1.png',
+  titulo:'Maquetacion web avanzada',
+  descripcion:'Primero pasos en el desarrollo de una pagina web responsive e implantación de animaciones y transiciones con CSS',
+  website:'https://affectionate-tesla-80eca8.netlify.app/',
+  github:'https://github.com/HelisMontes/web-avanzada'
+},{
+  id: '60908f30294b6d0015d06029',
+  img:'./img/portafolio-2.png',
+  titulo:'Maquetacion web básica',
+  descripcion:'Maquetación de un sitio web con HTML5 y CSS',
+  website:'https://condescending-payne-4074a5.netlify.app/',
+  github:'https://github.com/HelisMontes/web-basica'
+},{
+  id: '60908f4c294b6d0015d0602a',
+  img:'./img/portafolio-3.png',
+  titulo:'Administrar de citas',
+  descripcion:'El sitio web fue desarrollado usando TypeScript',
+  website:'https://confident-leavitt-f53e3d.netlify.app/',
+  github:'https://github.com/HelisMontes/project-administra-citas'
+},{
+  id: '60909322294b6d0015d0602d',
+  img:'./img/portafolio-5.png',
+  titulo:'Carrito de compras',
+  descripcion:'El sitio web fue desarrollado usando TypeScript y almacena la información en el localstorage',
+  website:'https://priceless-hypatia-2224d9.netlify.app/',
+  github:'https://github.com/HelisMontes/project-carrito-de-compra'
+},{
+  id: '60909439294b6d0015d0602f',
+  img:'./img/portafolio-6.png',
+  titulo:'Control de gastos',
+  descripcion:'El sitio web fue desarrollado usando TypeScript',
+  website:'https://youthful-jennings-8ad932.netlify.app/',
+  github:'https://github.com/HelisMontes/project-control-de-gastos'
+},{
+  id: '60909443294b6d0015d06030',
+  img:'./img/portafolio-7.png',
+  titulo:'Buscador de gifs',
+  descripcion:'Primero paso en el desarrollo de un sitio web con React.js',
+  website:'https://helismontes.github.io/02-git-expert-app/',
+  github:'https://github.com/HelisMontes/02-git-expert-app'
+},{
+  id: '60909460294b6d0015d06031',
+  img:'./img/portafolio-4.png',
+  titulo:'Diario',
+  descripcion:'Desarrollo de pagina web con React.js, Firebase y Redux',
+  website:'https://wonderful-heyrovsky-2525f6.netlify.app/',
+  github:'https://github.com/HelisMontes/05-JOURNAL-APP'
+},{
+  id: '60909475294b6d0015d06032',
+  img:'./img/portafolio-8.png',
+  titulo:'Calendario',
+  descripcion:'Desarrollo de pagina web con el stack MERN y Redux para el manejo de estados',
+  website:'https://mern-calendar-helis.herokuapp.com/',
+  github:'https://github.com/HelisMontes/calendar-app'
+}]
